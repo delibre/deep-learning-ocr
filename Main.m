f63e0ee19ed2f1ad9c4630a5d710d99a901286d8
Paweł Bielecki <pawelek24@op.pl>
2022-01-26 21:42:20 +0100

    Delay between serwer start and connecting to it

--- Main.m
+++ Main.m
@@ -1,8 +1,11 @@
 clear; close all; clc;
 fileName = input("Wpisz plik do przetworzenia: ");
 im = imread(fileName);
-letters = FindLetterImages(im);
 
 !python3 letter_classification/app.py letter_classification/enhanced-custom-dataset-model.h5 &
+
+letters = FindLetterImages(im);
+
+!sleep 3s
 
 client(letters)
