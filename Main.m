b5141a3f005b5780591ad7b7370e14c564817448
Paweł Bielecki <pawelek24@op.pl>
2022-01-27 14:53:46 +0100

    Chenged clinet.m to function

--- Main.m
+++ Main.m
@@ -2,7 +2,7 @@ clear; close all; clc;
 fileName = input("Wpisz plik do przetworzenia: ");
 im = imread(fileName);
 
-!python3 letter_classification/app.py letter_classification/enhanced-custom-dataset-model.h5 &
+% !python3 letter_classification/app.py letter_classification/enhanced-custom-dataset-model.h5 &
 
 letters = FindLetterImages(im);
 
--- client.m
+++ client.m
@@ -1,22 +1,29 @@
-import matlab.net.http.*
-import matlab.net.http.field.*
+function client(letters)
 
-data = jsondecode(fileread('test.json'));
-body = matlab.net.http.MessageBody(data);
-body.show
+    import matlab.net.http.*
+    import matlab.net.http.field.*
+    
+    data = jsondecode(mps.json.encode(letters));
+%     letter2Json(letters, 'test.json');
+%     data = jsondecode(fileread('test.json'));
+    
+    body = matlab.net.http.MessageBody(data);
+    body.show
+    
+    contentTypeField = matlab.net.http.field.ContentTypeField('application/json');
+    
+    type1 = matlab.net.http.MediaType('text/*');
+    type2 = matlab.net.http.MediaType('application/json','q','.5');
+    acceptField = matlab.net.http.field.AcceptField([type1 type2]);
+    
+    header = [acceptField contentTypeField];
+    
+    request = matlab.net.http.RequestMessage('POST',header,body);
+    
+    response = request.send('http://localhost:2640/predict');
+    
+    show(response);
+    
+    fprintf('%s',response.Body.Data.text);
 
-contentTypeField = matlab.net.http.field.ContentTypeField('application/json');
-
-type1 = matlab.net.http.MediaType('text/*');
-type2 = matlab.net.http.MediaType('application/json','q','.5');
-acceptField = matlab.net.http.field.AcceptField([type1 type2]);
-
-header = [acceptField contentTypeField];
-
-request = matlab.net.http.RequestMessage('POST',header,body);
-
-response = request.send('http://localhost:2640/predict');
-
-show(response);
-
-fprintf('%s',response.Body.Data.text);
+end
