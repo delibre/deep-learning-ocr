5aa7a6eb5395a88f4da72aea6bcb54b617df9c5f
Viacheslav <viacheslav.bylo@gmail.com>
2022-01-27 12:59:57 +0100

    changed client

--- client.m
+++ client.m
@@ -1,26 +1,22 @@
-function client(letters)
+import matlab.net.http.*
+import matlab.net.http.field.*
 
-    import matlab.net.http.*
-    import matlab.net.http.field.*
-    
-    data = jsondecode(mps.json.encode(letters)); 
-    body = matlab.net.http.MessageBody(data);
-    body.show
-    
-    contentTypeField = matlab.net.http.field.ContentTypeField('application/json');
-    
-    type1 = matlab.net.http.MediaType('text/*');
-    type2 = matlab.net.http.MediaType('application/json','q','.5');
-    acceptField = matlab.net.http.field.AcceptField([type1 type2]);
-    
-    header = [acceptField contentTypeField];
-    
-    request = matlab.net.http.RequestMessage('POST',header,body);
-    
-    response = request.send('http://localhost:2640/predict');
-    
-    show(response);
-    
-    fprintf('%c',char(response.Body.Data.code));
+data = jsondecode(fileread('test.json'));
+body = matlab.net.http.MessageBody(data);
+body.show
 
-end
+contentTypeField = matlab.net.http.field.ContentTypeField('application/json');
+
+type1 = matlab.net.http.MediaType('text/*');
+type2 = matlab.net.http.MediaType('application/json','q','.5');
+acceptField = matlab.net.http.field.AcceptField([type1 type2]);
+
+header = [acceptField contentTypeField];
+
+request = matlab.net.http.RequestMessage('POST',header,body);
+
+response = request.send('http://localhost:2640/predict');
+
+show(response);
+
+fprintf('%s',response.Body.Data.text);
